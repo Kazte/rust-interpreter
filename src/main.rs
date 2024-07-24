c93aa9520ab8c197d5ef47307238f82cbc1d93ed
Kazte <castellittofranco1@gmail.com>
2024-07-23 22:22:15 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use std::fs;
 use std::io::{self, Write};
 
 fn main() {
+    // Collect command line arguments
     let args: Vec<String> = env::args().collect();
+
+    // Check if the user has provided the correct number of arguments
     if args.len() < 3 {
+        // writeln vs println: writeln! is used to write to a buffer (io::stderr() in this case)
+        // println! is used to write to the standard output (stdout)
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
         return;
     }
@@ -17,17 +22,19 @@ fn main() {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
 
+            // Read the file contents
+            // unwrap_or_else is used to handle the error if the file is not found
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
             // Uncomment this block to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            // }
+            if !file_contents.is_empty() {
+                panic!("Scanner not implemented");
+            } else {
+                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            }
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
